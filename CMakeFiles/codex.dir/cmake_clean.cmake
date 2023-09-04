67b374d8b65fc6f3b8a813a7736238fdb6355783
Norbert Ormándi <60075235+DeszkaCodes@users.noreply.github.com>
2023-09-04 15:05:55 +0200

    Add cmake debug

---
+++ CMakeFiles/codex.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/codex.dir/src/main.cpp.o"
+  "CMakeFiles/codex.dir/src/main.cpp.o.d"
+  "codex"
+  "codex.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/codex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
