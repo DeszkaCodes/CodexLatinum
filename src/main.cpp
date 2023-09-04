2b0f7d2a6c5b251f362323a11f7d6e408db412d1
Norbert <60075235+DeszkaCodes@users.noreply.github.com>
2023-09-04 22:12:23 +0200

    Add token to assembly converter

--- src/main.cpp
+++ src/main.cpp
@@ -16,8 +16,54 @@ struct Token {
     std::optional<std::string> value;
 };
 
-std::queue<Token> tokenize(const std::string& source) {
-    std::queue<Token> token_queue;
+std::string tokens_to_asm(const std::vector<Token>& tokens) {
+    std::stringstream output;
+
+    output << "global _start\n";
+    output << "_start:\n";
+
+    size_t length = tokens.size();
+
+    for(size_t i = 0; i < length; i++) {
+        const Token& token = tokens.at(i);
+
+        switch (token.type)
+        {
+        case TokenType::_return:
+            if(i + 2 >= length) {
+                std::cerr << "too small tokens";
+                exit(EXIT_FAILURE);
+            }
+
+            if(tokens.at(i + 1).type != TokenType::int_lit) {
+                std::cerr << "not int";
+                exit(EXIT_FAILURE);
+            }
+
+            if(tokens.at(i + 2).type != TokenType::semicol) {
+                std::cerr << "missing ;";
+                exit(EXIT_FAILURE);
+            }
+
+            output << "    mov rax, 60\n";
+            output << "    mov rdi, " << tokens.at(i + 1).value.value() << "\n";
+            output << "    syscall\n";
+
+            i += 2;
+
+            break;
+        
+        default:
+            std::cerr << "error";
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return output.str().c_str();
+}
+
+std::vector<Token> tokenize(const std::string& source) {
+    std::vector<Token> token_vector;
 
     std::string buffer;
 
@@ -34,7 +80,7 @@ std::queue<Token> tokenize(const std::string& source) {
             }
 
             if(buffer == "reditus") {
-                token_queue.push({ .type = TokenType::_return });
+                token_vector.push_back({ .type = TokenType::_return });
                 buffer.clear();
             }
             else {
@@ -50,11 +96,11 @@ std::queue<Token> tokenize(const std::string& source) {
                 i++;
             }
 
-            token_queue.push({ .type = TokenType::int_lit, .value = buffer });
+            token_vector.push_back({ .type = TokenType::int_lit, .value = buffer });
             buffer.clear();
         }
         else if(c == ';') {
-            token_queue.push({ .type = TokenType::semicol });
+            token_vector.push_back({ .type = TokenType::semicol });
         }
         else if(std::isspace(c)){
             continue;
@@ -65,7 +111,7 @@ std::queue<Token> tokenize(const std::string& source) {
         }
     }
 
-    return token_queue;
+    return token_vector;
 }
 
 std::string read_file(const char* path) {
@@ -92,6 +138,10 @@ int main(int argc, char* argv[]) {
     std::string file_lines(read_file(argv[1]));
 
     auto tokens = tokenize(file_lines);
+
+    std::string assembly = tokens_to_asm(tokens);
+
+    std::cout << assembly << "\n";
 
     return EXIT_SUCCESS;
 }
