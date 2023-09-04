e0175656491b44227db5de671491b4b935599f1c
Norbert Ormándi <60075235+DeszkaCodes@users.noreply.github.com>
2023-09-04 13:12:23 +0200

    Add argument handling and file reading
    Added check to see if command contains 2 arguments.
    
    Added method to read in a file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include <sstream>
 
-int main() {
-    std::cout << "Hello world!" << std::endl;
-    return 0;
+std::string* read_file(const char* path) {
+    std::string* file_lines;
+    {
+        std::fstream input(path, std::ios::in);
+        std::stringstream contents_stream;
+
+        contents_stream << input.rdbuf();
+
+        input.close();
+
+        file_lines = &contents_stream.str();
+    }
+
+    return file_lines;
+}
+
+int main(int argc, char* argv[]) {
+
+    if(argc != 2) {
+        std::cerr << "Incorrect argument count.\n" << "Example usage: \"codex <file.cl>\"\n";
+        exit(EXIT_FAILURE);
+    }
+
+    std::string* file_lines(read_file(argv[1]));
+
+
+    return EXIT_SUCCESS;
 }
