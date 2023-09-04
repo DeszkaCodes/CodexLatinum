ad9bc0a13d194ecc1430f1bd2da2fb24aaebada2
Norbert <60075235+DeszkaCodes@users.noreply.github.com>
2023-09-04 21:55:54 +0200

    Add tokenization

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <sstream>
 #include <optional>
+#include <queue>
 
 enum class TokenType {
     _return,
@@ -15,6 +16,58 @@ struct Token {
     std::optional<std::string> value;
 };
 
+std::queue<Token> tokenize(const std::string& source) {
+    std::queue<Token> token_queue;
+
+    std::string buffer;
+
+    size_t length = source.length();
+    for(size_t i = 0; i < length; i++) {
+        char c = source.at(i);
+
+        if(std::isalpha(c)) {
+            buffer.push_back(c);
+
+            while(i + 1 < length && std::isalnum(source.at(i + 1))) {
+                buffer.push_back(source.at(i + 1));
+                i++;
+            }
+
+            if(buffer == "reditus") {
+                token_queue.push({ .type = TokenType::_return });
+                buffer.clear();
+            }
+            else {
+                std::cerr << "token error";
+                exit(EXIT_FAILURE);
+            }
+        }
+        else if(std::isdigit(c)) {
+            buffer.push_back(c);
+
+            while(i + 1 < length && std::isdigit(source.at(i + 1))) {
+                buffer.push_back(source.at(i + 1));
+                i++;
+            }
+
+            token_queue.push({ .type = TokenType::int_lit, .value = buffer });
+            buffer.clear();
+        }
+        else if(c == ';') {
+            token_queue.push({ .type = TokenType::semicol });
+        }
+        else if(std::isspace(c)){
+            continue;
+        }
+        else {
+            std::cerr << "syntax error";
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return token_queue;
+}
+
 std::string read_file(const char* path) {
     std::string file_lines;
     {
@@ -22,8 +75,6 @@ std::string read_file(const char* path) {
         std::stringstream contents_stream;
 
         contents_stream << input.rdbuf();
-
-        input.close();
 
         file_lines = contents_stream.str().c_str();
     }
@@ -39,6 +90,8 @@ int main(int argc, char* argv[]) {
     }
 
     std::string file_lines(read_file(argv[1]));
+
+    auto tokens = tokenize(file_lines);
 
     return EXIT_SUCCESS;
 }
